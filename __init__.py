20b543c1597a18f9667b8b07947b1fbd3469998b
hsnu122807 <100306001nccu@gmail.com>
2017-06-26 08:20:22 +0800

    Add files via upload
    發現cramming的錯誤
    註解掉選取最小k個residual的拿法
    
    註記了問題發生的地方
    可能要猜看看到底該怎麼給予output weight
    或是問蔡老師

--- __init__.py
+++ __init__.py
@@ -10,7 +10,7 @@ execute_start_time = time.time()
 
 # file_input = "binary_training_input_data"
 # file_output = "binary_training_output_data"
-data_amount = '10000'
+data_amount = '100'
 file_input = "tensorflow_binary_input_" + data_amount
 file_output = "tensorflow_binary_output_" + data_amount
 x_training_data = np.loadtxt(file_input + ".txt", dtype=float, delimiter=" ")
@@ -24,7 +24,7 @@ output_node_amount = 1
 learning_rate_eta = 0.01
 
 # Parameters
-thinking_times = 5000
+thinking_times = 50
 data_size = x_training_data.shape[0]
 big_number = 15
 
@@ -74,34 +74,50 @@ tool_sess.run([tool_init])
 
 for k in range(1, data_size + 1):
     print('-----stage: ' + str(k) + '-----')
+    '''
+    !!!!!!!!!!!重要!!!!!!!!!!!
+    tsaih(1993)的softening learning在cramming的時候必須確保除去最新一筆資料以外的所有資料已經符合condition L
+    否則cramming 無效
+    因為cramming這個方法只針對硬背一筆資料而已
+    
+    也因此不能挑residual最小的幾筆資料
+    因為可能不只需要調整一筆資料來滿足condition L
+    
+    應該問蔡老師在outlier detection的時候
+    硬背的部分該怎麼做到
+    會先確保除了最新的一筆(硬背目標)以外都滿足in envelope嗎?
+    '''
+    # if k == 1:
+    #     # get a random training case
+    #     r = random.randint(0, data_size-1)
+    #     current_stage_x_training_data = x_training_data[r-1:r]
+    #     current_stage_y_training_data = y_training_data[r-1:r]
+    # else:
+    #     # pick k data of smallest residual
+    #     predict_y = sess.run([output_layer],
+    #                          {x_placeholder: x_training_data,
+    #                           y_placeholder: y_training_data,
+    #                           tau_placeholder: tau_in_each_hidden_node})
+    #     squared_residuals = np.square(predict_y[0] - y_training_data.reshape((-1, 1)))
+    #     # print(squared_residuals)
+    #     concat_residual_and_x = np.concatenate((squared_residuals, x_training_data), axis=1)
+    #     concat_residual_and_y = np.concatenate((squared_residuals, y_training_data.reshape((data_size, output_node_amount))), axis=1)
+    #     sort_concat_x = concat_residual_and_x[np.argsort(concat_residual_and_x[:, 0])]
+    #     sort_concat_y = concat_residual_and_y[np.argsort(concat_residual_and_y[:, 0])]
+    #     x_training_data_sort_by_residual = np.delete(sort_concat_x, 0, axis=1)
+    #     y_training_data_sort_by_residual = np.delete(sort_concat_y, 0, axis=1)
+    #     # print(sort_concat_x)
+    #     # print(sort_concat_y)
+    #     # print(x_training_data_sort_by_residual)
+    #     # print(y_training_data_sort_by_residual)
+    #     current_stage_x_training_data = x_training_data_sort_by_residual[:k]
+    #     current_stage_y_training_data = y_training_data_sort_by_residual[:k]
+    #     # print(current_stage_x_training_data)
+    #     # print(current_stage_y_training_data)
 
-    if k == 1:
-        # get a random training case
-        r = random.randint(0, data_size-1)
-        current_stage_x_training_data = x_training_data[r-1:r]
-        current_stage_y_training_data = y_training_data[r-1:r]
-    else:
-        # pick k data of smallest residual
-        predict_y = sess.run([output_layer],
-                             {x_placeholder: x_training_data,
-                              y_placeholder: y_training_data,
-                              tau_placeholder: tau_in_each_hidden_node})
-        squared_residuals = np.square(predict_y[0] - y_training_data.reshape((-1, 1)))
-        # print(squared_residuals)
-        concat_residual_and_x = np.concatenate((squared_residuals, x_training_data), axis=1)
-        concat_residual_and_y = np.concatenate((squared_residuals, y_training_data.reshape((data_size, output_node_amount))), axis=1)
-        sort_concat_x = concat_residual_and_x[np.argsort(concat_residual_and_x[:, 0])]
-        sort_concat_y = concat_residual_and_y[np.argsort(concat_residual_and_y[:, 0])]
-        x_training_data_sort_by_residual = np.delete(sort_concat_x, 0, axis=1)
-        y_training_data_sort_by_residual = np.delete(sort_concat_y, 0, axis=1)
-        # print(sort_concat_x)
-        # print(sort_concat_y)
-        # print(x_training_data_sort_by_residual)
-        # print(y_training_data_sort_by_residual)
-        current_stage_x_training_data = x_training_data_sort_by_residual[:k]
-        current_stage_y_training_data = y_training_data_sort_by_residual[:k]
-        # print(current_stage_x_training_data)
-        # print(current_stage_y_training_data)
+    # take first k training case
+    current_stage_x_training_data = x_training_data[:k]
+    current_stage_y_training_data = y_training_data[:k]
 
     # calculate alpha & beta in condition L
     predict_y = sess.run([output_layer],
@@ -114,27 +130,33 @@ for k in range(1, data_size + 1):
     # check condition L
     class_1_output = [tf.double.max]
     class_2_output = [tf.double.min]
-    last_one_is_class_1 = False
     it = np.nditer(predict_y, flags=['f_index'])
-    i = 1
     while not it.finished:
         if current_stage_y_training_data[it.index] == 1:
             class_1_output.append(it[0])
-            if i == k:
-                last_one_is_class_1 = True
         else:  # if current_stage_y_training_data[it.index] == -1:
             class_2_output.append(it[0])
-        i += 1
         it.iternext()
     alpha, beta = tool_sess.run(
         [min_alpha, max_beta], {tool_alpha: class_1_output, tool_beta: class_2_output})
-    if last_one_is_class_1:
+    print('alpha = {0}   beta = {1}'.format(alpha, beta))
+    class_1_output = [tf.double.max]
+    class_2_output = [tf.double.min]
+    it = np.nditer(predict_y, flags=['f_index'])
+    while not it.finished:
+        if current_stage_y_training_data[it.index] == 1:
+            class_1_output.append(it[0])
+        else:  # if current_stage_y_training_data[it.index] == -1:
+            class_2_output.append(it[0])
+        it.iternext()
+    if current_stage_y_training_data[k - 1] == 1:  # new training case is class 1
         class_1_output = class_1_output[:-1]
     else:
         class_2_output = class_2_output[:-1]
     min_predict_value_in_class_one_of_previous_stage_training_case, max_predict_value_in_class_two_of_previous_stage_training_case = tool_sess.run(
         [min_alpha, max_beta], {tool_alpha: class_1_output, tool_beta: class_2_output})
-    print('alpha = {0}   beta = {1}'.format(alpha, beta))
+    print(min_predict_value_in_class_one_of_previous_stage_training_case)
+    print(max_predict_value_in_class_two_of_previous_stage_training_case)
 
     if alpha > beta:
         print('new training case is familiar to us, no further learning effort involved.')
@@ -148,6 +170,7 @@ for k in range(1, data_size + 1):
                                        y_placeholder: current_stage_y_training_data,
                                        tau_placeholder: tau_in_each_hidden_node})
             thinking_times_count += 1
+
             predict_y = sess.run([output_layer],
                                  {x_placeholder: current_stage_x_training_data,
                                   y_placeholder: current_stage_y_training_data,
@@ -155,15 +178,12 @@ for k in range(1, data_size + 1):
             # check condition L
             class_1_output = [tf.double.max]
             class_2_output = [tf.double.min]
-            last_one_is_class_1 = False
             it = np.nditer(predict_y, flags=['f_index'])
-            i = 1
             while not it.finished:
                 if current_stage_y_training_data[it.index] == 1:
                     class_1_output.append(it[0])
                 else:  # if current_stage_y_training_data[it.index] == -1:
                     class_2_output.append(it[0])
-                i += 1
                 it.iternext()
             alpha, beta = tool_sess.run(
                 [min_alpha, max_beta], {tool_alpha: class_1_output, tool_beta: class_2_output})
@@ -174,10 +194,10 @@ for k in range(1, data_size + 1):
             else:
                 if stage == (thinking_times-1):
                     thinking_failed = True
-                    print('thinking failed: after {0} times training, alpha still smaller than beta.'.format((i + 1)))
-                    # if needed, restore weight before cramming
-                    # print('restore weights.')
-                    # saver.restore(sess, r"C:\Users\Lee Chia Lun\PycharmProjects\autoencoder\softening_learning\model.ckpt")
+                    print('thinking failed: after {0} times training, alpha still smaller than beta.'.format((stage + 1)))
+                    # MUST restore before cramming(因為調權重可能會讓先前的資料違反condition L)
+                    print('restore weights.')
+                    saver.restore(sess, r"C:\Users\Lee Chia Lun\PycharmProjects\autoencoder\softening_learning\model.ckpt")
 
         if thinking_failed:
             # cram it first
@@ -197,18 +217,23 @@ for k in range(1, data_size + 1):
             print(current_output_weights)
             print('current output threshold:')
             print(current_output_threshold)
+            # calculate new hidden weight
             new_hidden_node_neuron_weights = current_stage_x_training_data[k - 1]
             print('new hidden weights:')
             print(new_hidden_node_neuron_weights)
+            # calculate new hidden threshold
             new_hidden_node_threshold = 1 - input_node_amount
             print('new hidden thresholds:')
             print(new_hidden_node_threshold)
+            # calculate new output weight
             if current_stage_y_training_data[k - 1] == 1:
-                new_output_node_neuron_weight = predict_y[0][
-                                                    k - 1] - max_predict_value_in_class_two_of_previous_stage_training_case
+                # 幹你的 這兩種都不對 到底???
+                # 研究stage52到底為啥會錯
+                # 難道還要判斷大小然後大減小???????
+                # new_output_node_neuron_weight = predict_y[0][k - 1] - max_predict_value_in_class_two_of_previous_stage_training_case
+                new_output_node_neuron_weight = max_predict_value_in_class_two_of_previous_stage_training_case - predict_y[0][k - 1]
             if current_stage_y_training_data[k - 1] == -1:
-                new_output_node_neuron_weight = min_predict_value_in_class_one_of_previous_stage_training_case - \
-                                                predict_y[0][k - 1]
+                new_output_node_neuron_weight = min_predict_value_in_class_one_of_previous_stage_training_case - predict_y[0][k - 1]
             print('predict value of most recent training case: ' + str(predict_y[0][k - 1]))
             print('new output weight:')
             print(new_output_node_neuron_weight)
@@ -254,7 +279,7 @@ for k in range(1, data_size + 1):
                 predict_y = sess.run([output_layer], {x_placeholder: current_stage_x_training_data,
                                                       y_placeholder: current_stage_y_training_data,
                                                       tau_placeholder: tau_in_each_hidden_node})
-                # print(predict_y)
+                print(predict_y)
 
                 # check condition L
                 class_1_output = [tf.double.max]
@@ -268,6 +293,7 @@ for k in range(1, data_size + 1):
                     it.iternext()
                 alpha, beta = tool_sess.run(
                     [min_alpha, max_beta], {tool_alpha: class_1_output, tool_beta: class_2_output})
+                print('after cramming, alpha = {0}, beta = {1}'.format(alpha, beta))
 
                 # softening
                 # save variables
@@ -283,7 +309,7 @@ for k in range(1, data_size + 1):
                     print(tau_in_each_hidden_node)
                     softening_success = False
 
-                    for i in range(thinking_times):
+                    for times in range(thinking_times):
                         # forward pass
                         predict_y = sess.run([output_layer], {x_placeholder: current_stage_x_training_data,
                                                               y_placeholder: current_stage_y_training_data,
@@ -310,7 +336,7 @@ for k in range(1, data_size + 1):
                             beta = test_beta
                             print(
                                 'softening success, gradient descent trained {0} times, #{1} tau value decrease by 1, current tau value: {2}'.format(
-                                    i, hidden_node_amount, newest_hidden_node_tau_value))
+                                    times, hidden_node_amount, newest_hidden_node_tau_value))
                             softening_success = True
                             saver.save(sess, r"{0}/model.ckpt".format(dir_path))
                             break
@@ -323,7 +349,7 @@ for k in range(1, data_size + 1):
                     if not softening_success:
                         print(
                             'softening failed, gradient descent trained {0} times, restore #{1} tau value , restore tau value: {2}'.format(
-                                i, hidden_node_amount, newest_hidden_node_tau_value + 1))
+                                times, hidden_node_amount, newest_hidden_node_tau_value + 1))
                         tau_in_each_hidden_node[hidden_node_amount - 1] = newest_hidden_node_tau_value + 1
                         saver.restore(sess, r"{0}/model.ckpt".format(dir_path))
                         break
@@ -422,16 +448,16 @@ for k in range(1, data_size + 1):
                             it.iternext()
                         exam_alpha, exam_beta = tool_sess.run(
                             [min_alpha, max_beta], {tool_alpha: class_1_output, tool_beta: class_2_output})
-                        print('*' * 5 + "exam removing hidden node #{}".format(remove_index) + '*' * 5)
-                        print("***** exam #{0} variables *****".format(remove_index))
-                        print(exam_h_w_val)
-                        print(exam_h_t_val)
-                        print(exam_o_w_val)
-                        print(exam_o_t_val)
-                        print("***** exam #{0} predict_y *****".format(remove_index))
-                        print(exam_predict_y)
+                        # print('*' * 5 + "exam removing hidden node #{}".format(remove_index) + '*' * 5)
+                        # print("***** exam #{0} variables *****".format(remove_index))
+                        # print(exam_h_w_val)
+                        # print(exam_h_t_val)
+                        # print(exam_o_w_val)
+                        # print(exam_o_t_val)
+                        # print("***** exam #{0} predict_y *****".format(remove_index))
+                        # print(exam_predict_y)
                         print("***** exam #{0} alpha & beta *****".format(remove_index))
-                        print(list(zip(exam_predict_y, current_stage_y_training_data)))
+                        # print(list(zip(exam_predict_y, current_stage_y_training_data)))
                         print('exam_alpha= ' + str(exam_alpha))
                         print('exam_beta= ' + str(exam_beta))
                         if exam_alpha <= exam_beta:
@@ -468,14 +494,61 @@ for k in range(1, data_size + 1):
                             # modify constant
                             tau_in_each_hidden_node = exam_tau
                             break
+    # if k == data_size:
+    #     new_path = r"{0}/".format(dir_path) + file_output
+    #     if not os.path.exists(new_path):
+    #         os.makedirs(new_path)
+    #
+    #     file = open(new_path + r"\_training_detail.txt", 'w')
+    #     file.writelines("learning_rate: " + str(learning_rate_eta) + "\n")
+    #     file.writelines("input_node_amount: " + str(input_node_amount) + "\n")
+    #     file.writelines("hidden_node_amount: " + str(hidden_node_amount) + "\n")
+    #     file.writelines("output_node_amount: " + str(output_node_amount) + "\n")
+    #     file.writelines("training_data_amount: " + str(data_size) + "\n")
+    #     file.writelines("alpha(class 1 min value): " + str(alpha) + "\n")
+    #     file.writelines("beta(class 2 max value): " + str(beta) + "\n")
+    #     file.writelines("thinking_times_count: " + str(thinking_times_count) + "\n")
+    #     file.writelines("cramming_times_count: " + str(cramming_times_count) + "\n")
+    #     file.writelines("softening_thinking_times_count: " + str(softening_thinking_times_count) + "\n")
+    #     file.writelines("pruning_success_times_count: " + str(pruning_success_times_count) + "\n")
+    #     file.writelines(
+    #         "total execution time: " + str(time.time() - execute_start_time) + " seconds" + "\n")
+    #     file.close()
+    #     curr_hidden_neuron_weight = sess.run([hidden_weights], {x_placeholder: x_training_data,
+    #                                                             y_placeholder: y_training_data,
+    #                                                             tau_placeholder: tau_in_each_hidden_node})
+    #     np.savetxt(new_path + r"\hidden_neuron_weight.txt", curr_hidden_neuron_weight)
+    #     curr_hidden_threshold = sess.run([hidden_thresholds],
+    #                                      {x_placeholder: x_training_data, y_placeholder: y_training_data,
+    #                                       tau_placeholder: tau_in_each_hidden_node})
+    #     np.savetxt(new_path + r"\hidden_threshold.txt", curr_hidden_threshold)
+    #     curr_output_neuron_weight = sess.run([output_weights], {x_placeholder: x_training_data,
+    #                                                             y_placeholder: y_training_data,
+    #                                                             tau_placeholder: tau_in_each_hidden_node})
+    #     np.savetxt(new_path + r"\output_neuron_weight.txt", curr_output_neuron_weight)
+    #     curr_output_threshold = sess.run([output_threshold],
+    #                                      {x_placeholder: x_training_data, y_placeholder: y_training_data,
+    #                                       tau_placeholder: tau_in_each_hidden_node})
+    #     np.savetxt(new_path + r"\output_threshold.txt", curr_output_threshold)
+    #
+    #     curr_average_loss = sess.run([average_squared_residual],
+    #                                  {x_placeholder: x_training_data, y_placeholder: y_training_data,
+    #                                   tau_placeholder: tau_in_each_hidden_node})
+    #     file.writelines("average_loss_of_the_model: " + str(curr_average_loss) + "\n")
+    #
+    #     np.savetxt(new_path + r"\tau_in_each_hidden_node.txt", tau_in_each_hidden_node)
+    #
+    #     print("--- execution time: %s seconds ---" % (time.time() - execute_start_time))
+
     if k == data_size:
-        new_path = r"{0}/".format(dir_path)+file_output
+        new_path = r"{0}/".format(dir_path) + file_output
         if not os.path.exists(new_path):
             os.makedirs(new_path)
         curr_hidden_neuron_weight, curr_hidden_threshold, curr_output_neuron_weight, curr_output_threshold, curr_average_loss, curr_output = sess.run(
             [hidden_weights, hidden_thresholds,
              output_weights, output_threshold, average_squared_residual,
-             output_layer], {x_placeholder: x_training_data, y_placeholder: y_training_data, tau_placeholder: tau_in_each_hidden_node})
+             output_layer],
+            {x_placeholder: x_training_data, y_placeholder: y_training_data, tau_placeholder: tau_in_each_hidden_node})
         np.savetxt(new_path + r"\hidden_neuron_weight.txt", curr_hidden_neuron_weight)
         np.savetxt(new_path + r"\hidden_threshold.txt", curr_hidden_threshold)
         np.savetxt(new_path + r"\output_neuron_weight.txt", curr_output_neuron_weight)
